6ebdc21caadcdd616878ce208c22415d89ba3908
Alexander Ley <alexanderley82@gmail.com>
2025-09-06 15:48:21 -0700

    Initial sharing of project

---
+++ PersonalInformation.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class GetToKnowYou here.
+ *
+ * @author Alexander Ley, aley@student.sdccd.edu
+ * @version V1.0
+ * @since 9/6/2025
+ */
+// This application prints out personal information using a single println statement.
+
+public class PersonalInformation {
+    public static void main(String[] args) {
+        
+        // Use a single println statement with newline characters (\n)
+        // to print each piece of information on a new line.
+        System.out.println("Alexander Ley\nCivil Engineering/GIS\n I have recently taken up fishing, I haven't caught anything yet! But I am having tons of fun doing it!");
+    }
+}
