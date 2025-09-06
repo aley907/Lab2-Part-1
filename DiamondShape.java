d529dd0e4fb53af695c5a8a8b90abe17b55b1878
aley907 <alexanderley82@gmail.com>
2025-09-06 16:38:38 -0700

    Add files via upload

---
+++ DiamondShape.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Diamond here.
+ *
+ * @author Alexander Ley, aley@student.sdccd.edu
+ * @version V1.0
+ * @since 9/6/2025
+ */
+public class DiamondShape {
+    public static void main(String[] args) {
+        // Top half of the diamond
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
+        
+        // Bottom half of the diamond
+        System.out.println(" *****");
+        System.out.println("  ***");
+        System.out.println("   *");
+    }
+}
